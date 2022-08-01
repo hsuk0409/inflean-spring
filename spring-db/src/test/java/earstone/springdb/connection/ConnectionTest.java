f9d93160b6d3c1c03674e5c470100139ef39b40f
chionsuk <hsuk0409@gmail.com>
2022-08-01 22:01:38 +0900

    DataSource 사용

---
+++ spring-db/src/test/java/earstone/springdb/connection/ConnectionTest.java
@@ -0,0 +1,42 @@
+package earstone.springdb.connection;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import static earstone.springdb.connection.ConnectionConst.*;
+
+@Slf4j
+public class ConnectionTest {
+
+    @DisplayName("드라이버 매니저의 커넥션 두개 생성해서 출력해본다.")
+    @Test
+    void driverManager() throws SQLException {
+        Connection con1 = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        Connection con2 = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        log.info("connection={}, class={}", con1, con1.getClass());
+        log.info("connection={}, class={}", con2, con2.getClass());
+    }
+
+    @DisplayName("DataSource 사용해서 커넥션을 획득한다.")
+    @Test
+    void dataSourceDriverManager() throws SQLException {
+        // DriverManagerDataSource - 항상 새로운 커넥션을 획득
+        DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
+        userDataSource(dataSource);
+    }
+
+    private void userDataSource(DataSource dataSource) throws SQLException {
+        Connection con1 = dataSource.getConnection();
+        Connection con2 = dataSource.getConnection();
+        log.info("connection={}, class={}", con1, con1.getClass());
+        log.info("connection={}, class={}", con2, con2.getClass());
+    }
+
+}
