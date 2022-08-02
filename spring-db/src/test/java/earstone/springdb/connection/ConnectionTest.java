e7c61be7d181c048da9ba53e6389f92dab05a5d4
chionsuk <hsuk0409@gmail.com>
2022-08-02 23:54:01 +0900

    데이터소스 커넥션 풀

--- spring-db/src/test/java/earstone/springdb/connection/ConnectionTest.java
+++ spring-db/src/test/java/earstone/springdb/connection/ConnectionTest.java
@@ -1,5 +1,6 @@
 package earstone.springdb.connection;
 
+import com.zaxxer.hikari.HikariDataSource;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -30,6 +31,21 @@ public class ConnectionTest {
         // DriverManagerDataSource - 항상 새로운 커넥션을 획득
         DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
         userDataSource(dataSource);
+    }
+
+    @DisplayName("")
+    @Test
+    void dataSourceConnectionPool() throws SQLException, InterruptedException {
+        // 커넥션 풀링
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(URL);
+        dataSource.setUsername(USERNAME);
+        dataSource.setPassword(PASSWORD);
+        dataSource.setMaximumPoolSize(10);
+        dataSource.setPoolName("JustinPool");
+
+        userDataSource(dataSource);
+        Thread.sleep(1000);
     }
 
     private void userDataSource(DataSource dataSource) throws SQLException {
